eb8c3911627ca5e77ba4fad377e288d8c453c533
sour-soup <kiziyepi8@gmail.com>
2025-05-17 17:23:58 +0400

    BIM BIM BAM BAM!!!!!!!!!

--- src/main/java/errorhack/mainservice/controller/ClusterController.java
+++ src/main/java/errorhack/mainservice/controller/ClusterController.java
@@ -1,14 +1,12 @@
 package errorhack.mainservice.controller;
 
-import errorhack.mainservice.dto.ClusterDto;
 import errorhack.mainservice.dto.VersionDto;
+import errorhack.mainservice.model.UrlRequest;
 import errorhack.mainservice.service.ClusterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +14,9 @@ public class ClusterController {
 
     private final ClusterService clusterService;
 
-    @GetMapping("/clusters/{url}")
-    public VersionDto getClusters(@PathVariable String url) {
-        return clusterService.getClusters(url);
+    @PostMapping("/clusters/{url}")
+    public VersionDto getClusters(@RequestBody UrlRequest url) {
+        return clusterService.getClusters(url.url());
     }
 
 }
