3320707b36d562357afb0028d313c59b4f8d586c
Kirill <fikfack806@mail.ru>
2025-05-17 18:17:07 +0400

    dibilniy bug fixed

--- src/main/java/errorhack/mainservice/client/LLMClient.java
+++ src/main/java/errorhack/mainservice/client/LLMClient.java
@@ -1,15 +1,11 @@
 package errorhack.mainservice.client;
 
 import errorhack.mainservice.model.AskRequest;
-import errorhack.mainservice.model.ParsingEntry;
-import errorhack.mainservice.response.MessageResponse;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
-import java.util.List;
 
 public interface LLMClient {
-    @GetExchange("/ask")
+    @PostExchange("/ask")
     String ask(@RequestBody AskRequest request);
 }
--- src/main/java/errorhack/mainservice/config/RestClientConfig.java
+++ src/main/java/errorhack/mainservice/config/RestClientConfig.java
@@ -1,7 +1,9 @@
 package errorhack.mainservice.config;
 
+import errorhack.mainservice.client.LLMClient;
 import errorhack.mainservice.client.ParsingClient;
 import errorhack.mainservice.client.properties.AbstractRestClientProperties;
+import errorhack.mainservice.client.properties.LLMRestClientProperty;
 import errorhack.mainservice.client.properties.ParsingRestClientProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,7 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
-@EnableConfigurationProperties(ParsingRestClientProperty.class)
+@EnableConfigurationProperties({ParsingRestClientProperty.class, LLMRestClientProperty.class})
 public class RestClientConfig {
 
     @Bean
@@ -23,6 +25,16 @@ public class RestClientConfig {
                 HttpServiceProxyFactory.builderFor(adapter).build();
 
         return factory.createClient(ParsingClient.class);
+    }
+    @Bean
+    public LLMClient llmClient(RestClient.Builder builder, LLMRestClientProperty properties) {
+        RestClient restClient = buildBaseRestClient(builder, properties).build();
+
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(LLMClient.class);
     }
 
     private RestClient.Builder buildBaseRestClient(
